d4303fab8e819e95f8c3e6d09409947d110b6dcb
Rodrigo Amaro <rodrigo@amaro.dev>
2020-05-18 11:27:05 -0500

    First attempt

---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const request = require('node-fetch')
+const fs = require('fs')
+
+async function run() {
+    try {
+        const nowSecureToken = core.getInput('token');
+        const filePath = core.getInput('artifact_path')
+        const stats = fs.statSync(filePath);
+        const fileSizeInBytes = stats.size;
+        let readStream = fs.createReadStream(filePath);
+        await fetch('https://lab-api.nowsecure.com/build/', {
+            method: 'POST',
+            headers: {
+                "Authorization": "Bearer " + nowSecureToken,
+                "Content-length": fileSizeInBytes
+            },
+            body: readStream
+        }).then(data => data.json())
+            .then(resp => {
+                console.log(resp)
+            })
+            .catch(error => core.setFailed(error.message))
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run()
